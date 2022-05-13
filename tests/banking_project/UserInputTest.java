6684cfd541589a59be2409f2d4c52b1d7af82eba
Liran Hadad <liranh6@gmail.com>
2022-05-13 19:17:57 +0300

    setting test folder

---
+++ tests/banking_project/UserInputTest.java
@@ -0,0 +1,14 @@
+package banking_project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserInputTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
